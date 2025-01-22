b978310c0224c2c8c48cd22d638f03a8728524e3
Luiz Gustavo Silva <100055183+luizgsilva1@users.noreply.github.com>
2025-01-22 18:59:31 -0300

    secao 6
    Início da seção 6 sobre funções

---
+++ secao6/src/curso/luizgustavo/funcoes/Funcoes.java
@@ -0,0 +1,14 @@
+package curso.luizgustavo.funcoes;
+
+public class Funcoes {
+    public static void main(String[] args) {
+
+        saudacao();
+        saudacao();
+
+    }
+
+    public static void saudacao() {
+        System.out.println("Olá, esta é minha primeira função!");
+    }
+}
