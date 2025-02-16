cc65c000961f4ea544f1453eb708a34fb291b868
Luiz Gustavo Silva <100055183+luizgsilva1@users.noreply.github.com>
2025-02-15 22:48:07 -0300

    secao 6
    Continuando estudo sobre funções em java

--- secao6/src/curso/luizgustavo/funcoes/Funcoes.java
+++ secao6/src/curso/luizgustavo/funcoes/Funcoes.java
@@ -1,5 +1,8 @@
 package curso.luizgustavo.funcoes;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Funcoes {
     public static void main(String[] args) {
 
@@ -7,6 +10,21 @@ public class Funcoes {
         saudacao();
         pergunta();
 
+        //PARAMENTROS
+
+        soma(4, 2);
+
+        //RETURN
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ROOT);
+
+        System.out.println("Insira um número inteiro: ");
+        int verifica = scanner.nextInt();
+
+        System.out.println(verificaPar(verifica));
+
+        scanner.close();
     }
 
     public static void saudacao() {
@@ -15,5 +33,18 @@ public class Funcoes {
 
     public static void pergunta() {
         System.out.println("Como foi seu dia?");
+    }
+
+    public static void soma(int a, int b) {
+        int res = a + b;
+        System.out.println("O resultado da função é:" + res);
+    }
+
+    public static String verificaPar(int numeroVer) {
+        if (numeroVer % 2 == 0) {
+            return "O número " + numeroVer + " é par.";
+        } else {
+            return "O número " + numeroVer + " é ímpar.";
+        }
     }
 }
